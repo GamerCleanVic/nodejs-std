0f45553440740851d0abb27ec889226aac337865
GamerCleanVic <jonassd2000@gmail.com>
2023-09-12 18:30:14 -0500

    mustache

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,13 @@ import express, { Request, Response } from 'express';
 import painelRoutes from './routes/painel';
 import mainRoutes from './routes/index';
 import path from 'path';
+import mustache from 'mustache-express';
 
 const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
 
 server.use('/static', express.static(path.
     join(__dirname, '../public')));
