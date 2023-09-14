bb747a75013281e201c4b2b9961824bae7dbba1b
GamerCleanVic <jonassd2000@gmail.com>
2023-09-13 20:47:49 -0500

    post

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -33,4 +33,37 @@ router.get('/sobre', (req: Request, res: Response)=>{
     res.render('pages/sobre');
 });
 
+router.get('/nome', (req: Request, res: Response)=>{
+    console.log("query string", req.query);
+
+    let nome: string = req.query.nome as string;
+    let idade: string = req.query.idade as string;
+
+    res.render('pages/nome', {
+        nome,
+        idade
+    });
+});
+
+router.get('/idade', (req: Request, res: Response)=>{ 
+    res.render('pages/idade');
+});
+
+router.post('/idade',(req: Request, res:Response)=>{
+    let mostrarIdade: boolean = false;
+    let idade: number = 0;
+
+    if(req.body.ano){
+        let anoNascimento: number = parseInt(req.body.ano as string);
+        let anoAtual: number = new Date().getFullYear();
+        idade = anoAtual - anoNascimento;
+        mostrarIdade = true;
+    }
+
+    res.render('pages/idade', {
+        idade,
+        mostrarIdade
+    });
+});
+
 export default router;
